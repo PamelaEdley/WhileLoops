4ed665f320748b428dbdc6a23e2bec4f15e9ab48
A252430 <A252430@LSBSA-8NM5HW3>
2023-12-13 16:52:24 +0200

    Add project files.

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1._15_while_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            while (index < 5)
+            {
+
+                Console.WriteLine(index.ToString());
+                index++;
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
